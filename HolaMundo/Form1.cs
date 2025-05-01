f8099d374c36bb1f543f4058243a6cc9b1fc2422
Oswaldo Ramos <hector.ramos9290@alumnos.udg.mx>
2025-04-30 18:42:29 -0600

    Add comments

--- HolaMundo/Form1.cs
+++ HolaMundo/Form1.cs
@@ -12,10 +12,19 @@ namespace HolaMundo
 
         private async void btnSubmit_Click(object sender, EventArgs e)
         {
+            // Show progress bar
             progressBar1.Visible = true;
             // 2 second delay to show progress bar
             await Task.Delay(2000);
+            /*
+             * Regex password validation
+             * - At least one lowercase letter
+             * - At least one uppercase letter
+             * - At least one digit
+             * - At least one special character
+             */
             bool isValid = Regex.IsMatch(txtPass.Text, @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$");
+            // Check if both passwords are the same and if matches the pattern
             if (txtPass.Text == txtConfirmPassword.Text && isValid)
             {
                 // Open a message for validation ok
@@ -23,8 +32,10 @@ namespace HolaMundo
             }
             else
             {
+                // Open a message for validation not ok
                 MessageBox.Show("Password is not valid");
             }
+            // Hide progress bar
             progressBar1.Visible = false;
         }
     }
